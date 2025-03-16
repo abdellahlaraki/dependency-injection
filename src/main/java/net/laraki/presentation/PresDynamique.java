dca82c5705e259a7ef5c8e21fd62cf8cae4f1543
abdellah laraki <abdellahlaraki16@gmail.com>
2025-03-16 12:21:46 +0000

    prsentation dynamique avec constructeur et setter

--- src/main/java/net/laraki/presentation/PresDynamique.java
+++ src/main/java/net/laraki/presentation/PresDynamique.java
@@ -6,19 +6,25 @@ import net.laraki.metier.IMetier;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Scanner;
-
 public class PresDynamique {
-    public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    //FileNotFoundException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException
+    public static void main(String[] args) throws Exception {
         Scanner scanner=new Scanner(new File("config.txt"));
-
         String classDao=scanner.nextLine();
         Class Cdao=Class.forName(classDao);
         IDao dao=(IDao) Cdao.newInstance();
-
         String classMetier=scanner.nextLine();
         Class cMetier=Class.forName(classMetier);
-        IMetier metier=(IMetier) cMetier.getConstructor(IDao.class).newInstance(dao);
+
+        // injection de dépendence par constructeur
+        //IMetier metier=(IMetier) cMetier.getConstructor(IDao.class).newInstance(dao);
+
+        // injection de dépendence par setter
+        IMetier metier=(IMetier) cMetier.getConstructor().newInstance();
+        Method setDao=cMetier.getDeclaredMethod("setDao",IDao.class);
+        setDao.invoke(metier,dao);
         System.out.println(metier.calcul());
     }
 }
